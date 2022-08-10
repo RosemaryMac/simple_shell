6e4356d93345462c62b47284ef7346938a5907c1
RosemaryMac <rmachanja14@gmail.com>
2022-08-10 12:45:15 -0700

    shell

--- shell.c
+++ shell.c
@@ -1,54 +1,106 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-
-#define PRMTSIZ 255
-#define MAXARGS 63
-#define EXITCMD "exit"
+#include "shell.h"
 
 /**
- * main - Entry point
- * *ptr: variable pointer
- * args: Arguments
- *
+ * main - A function that runs our shell
+ * @ac: The number of inputed arguments.
+ * @av: The pointer to array of inputed arguments.
+ * @env: The pointer to array of enviromental variables.
  * Return: Always 0
  */
-int main(void)
+int main(int ac, char **av, char **env)
 {
-	for (;;)
+	char *buffer = NULL, **command = NULL;
+	size_t buf_size = 0;
+	ssize_t chars_readed = 0;
+	int cicles = 0;
+	(void)ac;
+
+	while (1)
 	{
-		char input[PRMTSIZ + 1] = { 0x0 };
-		char *ptr = input;
-		char *args[MAXARGS + 1] = { NULL };
-		int wstatus;
-
-		printf("%s ", getuid() == 0 ? "#" : "$");
-		fgets(input, PRMTSIZ, stdin);
-
-		if (*ptr == '\n')
-			continue;
-
-		for (size_t i = 0; i < sizeof(args) && *ptr; ptr++)
+		cicles++;
+		prompt();
+		signal(SIGINT, handle);
+		chars_readed = getline(&buffer, &buf_size, stdin);
+		if (chars_readed == EOF)
+			_EOF(buffer);
+		else if (*buffer == '\n')
+			free(buffer);
+		else
 		{
-			if (*ptr == ' ')
-				continue;
-			if (*ptr == '\n')
-				break;
-			for (args[i++] = ptr; *ptr && *ptr != ' ' && *ptr != '\n'; ptr++);
+			buffer[_strlen(buffer) - 1] = '\0';
+			command = tokening(buffer, " \0");
+			free(buffer);
+			if (_strcmp(command[0], "exit") != 0)
+				shell_exit(command);
+			else if (_strcmp(command[0], "cd") != 0)
+				change_dir(command[1]);
+			else
+				create_child(command, av[0], env, cicles);
 		}
-
-		if (strcmp(EXITCMD, args[0]) == 0)
-			return (0);
-
-		signal(SIGINT, SIG_DFL);
-		if (fork() == 0)
-			exit(execvp(args[0], args));
-		signal(SIGINT, SIG_IGN);
-
-		wait(&wstatus);
-		if (WIFEXITED(wstatus))
-			printf("<%d>", WEXITSTATUS(wstatus));
+		fflush(stdin);
+		buffer = NULL, buf_size = 0;
 	}
+	if (chars_readed == -1)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
+
+/**
+ * prompt - A function that prints the prompt
+ * Return: Nothing.
+ */
+void prompt(void)
+{
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "Hell_Shell>> ", 13);
+}
+
+/**
+ * handle - A function to handle Ctr + C signal.
+ * @signals: The signal to handle.
+ * Return: Nothing.
+ */
+void handle(int signals)
+{
+	(void)signals;
+	write(STDOUT_FILENO, "\nHell_Shell>> ", 14);
+}
+
+/**
+ * _EOF - A function that checks if buffer is EOF
+ * @buffer: The pointer to the input string.
+ * Return: Nothing
+ */
+void _EOF(char *buffer)
+{
+	if (buffer)
+	{
+		free(buffer);
+		buffer = NULL;
+	}
+
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "\n", 1);
+	free(buffer);
+	exit(EXIT_SUCCESS);
+}
+
+/**
+ * shell_exit - A function that exits the shell.
+ * @command: The pointer to tokenized command.
+ * Return: Nothing.
+ */
+void shell_exit(char **command)
+{
+	int sta_tus = 0;
+
+	if (command[1] == NULL)
+	{
+		free_dp(command);
+		exit(EXIT_SUCCESS);
+	}
+	
+	sta_tus = _atoi(command[1]);
+	free_dp(command);
+	exit(sta_tus);
 }
